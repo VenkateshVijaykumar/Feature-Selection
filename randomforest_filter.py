d2850b1d518ee7d812db5d42744695df0577beca
VenkateshVijaykumar <venkatesh.vijaykumar93@gmail.com>
2018-12-17 11:41:41 +0530

    First Commit

---
+++ randomforest_filter.py
@@ -0,0 +1,41 @@
+"""Authored by: Venkatesh Vijaykumar github: @VenkateshVijaykumar
+   This implements Random Forests as a Feature Selection Method.
+   The tree method used is the ensemble Random Forests method from sklearn.
+   The rf_filter_method acts as a feature 'filter' which performs feature selection based
+   on the importance of the feature in the splits of the trees in the forest.
+   The 'filter' has no interaction with the learner that uses the reduced feature set.
+   The dataset used here in the main section is the Iris dataset also from sklearn.
+   Requires Numpy and sklearn to run.
+"""
+"""To run:
+   import the filter_method from this dt_filter.py file into your own file.
+   pass the features and classes in array formats [n_samples,n_features] for feature array and
+   [n_samples] or [n_samples,n_outputs] for class array.
+   You can vary the percent_reduction parameter to reflect the percentage of original features
+   to be retained. The retained percentage will include the most important n% of the original
+   You can vary the number of weak learners in the ensemble with the n_est parameter
+"""
+
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from matplotlib import pyplot as plt
+
+def rf_filter_method(features,classes,percent_reduction=0.5,n_est=10):
+    clf = RandomForestClassifier(n_estimators = n_est, criterion = "gini", random_state = 100, max_depth=None, min_samples_leaf=5)
+    clf.fit(features, classes)
+    filtered_features = np.argsort(clf.feature_importances_).tolist()
+    filtered_features = filtered_features[::-1]
+    num_features = int(np.ceil(percent_reduction * len(filtered_features)))
+    final_features = filtered_features[:num_features]
+    new_features = features[:,final_features][:]
+    return new_features, np.asarray(filtered_features)
+
+if __name__ == '__main__':
+    iris = load_iris()
+    train_feat = iris.data
+    train_class = iris.target
+    print "Original feature space shape is: ", train_feat.shape
+    new_train_feat, sorted_features = rf_filter_method(train_feat,train_class)
+    print "Features in order of importance are: ", sorted_features
+    print "Reduced feature space shape is: ", new_train_feat.shape
---
+++ wrapper.py
@@ -0,0 +1,52 @@
+"""Authored by: Venkatesh Vijaykumar github: @VenkateshVijaykumar
+   This implements Extra Trees as a Feature Selection Method.
+   The tree method used is the ensemble Extra Trees method from sklearn.
+   The et_filter_method acts as a feature 'filter' which performs feature selection based
+   on the importance of the feature in the splits of the trees in the forest.
+   The 'filter' has no interaction with the learner that uses the reduced feature set.
+   The dataset used here in the main section is the Iris dataset also from sklearn.
+   Requires Numpy and sklearn to run.
+"""
+"""To run:
+   import the filter_method from this dt_filter.py file into your own file.
+   pass the features and classes in array formats [n_samples,n_features] for feature array and
+   [n_samples] or [n_samples,n_outputs] for class array.
+   You can vary the percent_reduction parameter to reflect the percentage of original features
+   to be retained. The retained percentage will include the most important n% of the original
+   You can vary the number of weak learners in the ensemble with the n_est parameter
+"""
+
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+from matplotlib import pyplot as plt
+from itertools import combinations
+
+def et_filter_method(features,classes,estimator,percent_reduction=0.5,n_est=10):
+    idx_lst = []
+    combs = []
+    acc_lst = []
+    for i in range(features.shape[1]):
+        idx_lst.append(i)
+    for i in xrange(1, len(idx_lst)+1):
+        els = [list(x) for x in combinations(idx_lst, i)]
+        combs.extend(els)
+    for i in range(len(combs)):
+        new_features = features[:,combs[i]][:]
+        estimator.fit(new_features,classes)
+        predictions = estimator.predict(new_features)
+        accuracy = accuracy_score(classes,predictions)*100
+        acc_lst.append([combs[i],accuracy])
+    best_comb, best_acc = max(acc_lst, key=lambda x:x[1])
+    best_features = features[:,best_comb][:]
+    return best_features, acc_lst
+    
+if __name__ == '__main__':
+    iris = load_iris()
+    train_feat = iris.data
+    train_class = iris.target
+    estimator = MLPClassifier(hidden_layer_sizes=100, max_iter=600, alpha=0.1, learning_rate='constant',
+                    solver='adam', verbose=0, tol=1e-4, random_state=1, learning_rate_init=.05)
+    best_combo, accuracies = et_filter_method(train_feat,train_class,estimator)
+    print accuracies
